2538b502204ae6e5e70f4e2c86791ecd48e59d5b
Joao A. Duro <joao.duro@gmail.com>
2021-01-02 17:56:10 +0000

    Update to year 2021

--- src/libs/tigon/Utils/Dominance/PreferabilityConstraintHandling.cpp
+++ src/libs/tigon/Utils/Dominance/PreferabilityConstraintHandling.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2012-2020 The University of Sheffield (www.sheffield.ac.uk)
+** Copyright (C) 2012-2021 The University of Sheffield (www.sheffield.ac.uk)
 **
 ** This file is part of Liger.
 **
