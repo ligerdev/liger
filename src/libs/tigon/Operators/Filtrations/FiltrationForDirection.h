8a9e58f8c1d2ae4a7d4149692f332917dc00091d
Joao A. Duro <j.a.duro@sheffield.ac.uk>
2021-04-27 22:36:11 +0100

    Upgrade all files from LGPL 2.1 to version 3

--- src/libs/tigon/Operators/Filtrations/FiltrationForDirection.h
+++ src/libs/tigon/Operators/Filtrations/FiltrationForDirection.h
@@ -6,11 +6,11 @@
 **
 ** GNU Lesser General Public License Usage
 ** This file may be used under the terms of the GNU Lesser General
-** Public License version 2.1 as published by the Free Software
+** Public License version 3 as published by the Free Software
 ** Foundation and appearing in the file LICENSE.LGPL included in the
 ** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: http://www.gnu.org/licenses/lgpl-3.0.html.
 **
 ****************************************************************************/
 #ifndef FILTRATIONFORDIRECTION_H
